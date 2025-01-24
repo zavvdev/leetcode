d8c0c0f75b8bd69d016458715a1a818026362707
zavvdev <zaviryukha.igor99@gmail.com>
2025-01-24 19:24:07 +0200

    str_index_of

---
+++ str_index_of.js
@@ -0,0 +1,35 @@
+/**
+ * Given two strings needle and haystack, return the index of the first occurrence
+ * of needle in haystack, or -1 if needle is not part of haystack.
+ */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @returns {number}
+ */
+var str_index_of = function(haystack, needle) {
+  if (haystack.length < needle.length) return -1;
+  if (haystack === needle) return 0;
+
+  outer: for (let h = 0; h < haystack.length; h++) {
+    if (haystack[h] === needle[0]) {
+      for (let n = 0; n < needle.length; n++)
+        if (haystack[h + n] !== needle[n]) continue outer;
+
+      return h;
+    }
+  }
+
+  return -1;
+};
+
+str_index_of("leetcode", "leet"); // 0
+str_index_of("leetcode", "tco"); // 3
+str_index_of("leetcode", "code"); // 4
+str_index_of("leetcode", "codee"); // -1
+str_index_of("leet", "missing"); // -1
+str_index_of("leet", "leet"); // 0
+str_index_of("leetcode", "leeto"); // -1
+str_index_of("abc", "c"); // 2
+str_index_of("mississippi", "issip"); // 4
