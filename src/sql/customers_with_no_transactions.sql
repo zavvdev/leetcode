e59d8d9ad1445ba419d31293375468ff8cd492b2
zavvdev <zaviryukha.igor99@gmail.com>
2025-02-22 16:30:46 +0200

    sql

---
+++ src/sql/customers_with_no_transactions.sql
@@ -0,0 +1,34 @@
+/*
+Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+*/
+
+SELECT l.customer_id, COUNT(*) AS count_no_trans FROM Visits AS l
+WHERE l.visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY l.customer_id;
---
+++ src/sql/rising_temp.sql
@@ -0,0 +1,25 @@
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+*/
+
+SELECT t.id
+FROM Weather y 
+INNER JOIN Weather t
+WHERE DATEDIFF(t.recordDate, y.recordDate) = 1
+AND t.temperature > y.temperature;
